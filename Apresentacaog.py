07713d58dce8d4336a42c56a416a2bf748037de5
controladoria_para_voce <controladoria@alldax.com>
2023-04-26 10:10:56 -0300

    Commit_004

--- Apresentacaog.py
+++ Apresentacaog.py
@@ -55,7 +55,7 @@ class apresentacao():
     def __init__(self):
     
         #self.base_extratos = pd.read_excel(r"T:\CLIENTES\GRUPO H2F\CONTROLADORIA\Extratos Jan-Mar.xlsx")
-        self.logoalldaxreduzida = Image.open(r"Logos\logoalldaxred.png")
+        self.logoalldaxreduzida = Image.open(r"logoalldaxred.png")
         self.logoh2f = Image.open(r"Logos\logoh2f.png")
         self.logos = Image.open(r"Logos\logosalt.png")
         self.resultadosicone = Image.open(r"Logos\resukt.png")
